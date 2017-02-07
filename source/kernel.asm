8303c09cd1661e02988ae91682f0e912d1ef38a9
rsimari <rsimari@nd.edu>
2017-02-07 16:14:47 -0500

    updated stuff

--- source/kernel.asm
+++ source/kernel.asm
@@ -26,25 +26,6 @@ os_main:
 	mov ax, 2000h
 	mov ds, ax     ; sets data segment to 2000h where it was loaded. 
 
-	mov ax, 2000h			; Set all segments to match where kernel is loaded
-	; mov ds, ax			; After this, we don't need to bother with
-	; mov es, ax			; segments ever again, as MikeOS and its programs
-	; mov fs, ax			; live entirely in 64K
-	; mov gs, ax			; es, fs, gs are extra segments for stuff like memory
-
-	cmp dl, 0
-	je no_change
-	mov [bootdev], dl		; Save boot device number
-	push es 			; pushes es onto stack 
-	mov ah, 8			; Get drive parameters
-	int 13h				; 13h for write string
-	pop es				; pops es back off of stack
-	and cx, 3Fh			; Maximum sector number
-	mov [SecsPerTrack], cx		; Sector numbers start at 1
-	movzx dx, dh			; Maximum head number
-	add dx, 1			; Head numbers start at 0 - add 1 for total
-	mov [Sides], dx
-
 	mov ax, 1003h			; Set text output with certain attributes
 	mov bx, 0			; to be bright, and not blinking
 	int 10h				; sets more pallette registers
