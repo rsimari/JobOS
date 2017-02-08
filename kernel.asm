6cc3facf6d4eb194f91de169ca72bb7b468ce64e
rsimari <rsimari@nd.edu>
2017-02-08 11:57:22 -0500

    added message and new line

--- kernel.asm
+++ kernel.asm
@@ -14,10 +14,34 @@ os_main:
 
 	mov si, 0
 	mov si, kernel_load
-	call os_print_string	
-
+	call os_clear_screen
+	call os_print_string
+	;call os_write_char_at_cursor
+	call os_move_cursor
 
 	jmp $
+
+os_clear_screen:
+	pusha
+
+	mov dx, 0
+
+	; move curser to top
+	mov bh, 0
+	mov ah, 2
+	int 10h
+	; end move curser
+
+	mov ah, 6
+	mov al, 0
+	mov bh, 7
+	mov cx, 0
+	mov dh, 24
+	mov dl, 79
+	int 10h
+
+	popa
+	ret
 
 os_print_string:
 	pusha
@@ -30,8 +54,9 @@ os_repeat:
 	je os_done
 	int 10h
 	jmp short os_repeat
-os_done: 
 
+os_done: 
+	call os_new_line
 	popa 
 	ret 
 
@@ -39,8 +64,31 @@ os_end:
 	mov ah, 00h
 	;int 19h
 
+os_new_line:
+	pusha
 
-kernel_load db "hello", 0
+	mov ah, 02h
+	inc dh      ; increments the row number
+	mov dl, 00h
+	int 10h
+
+	popa
+	ret
+
+os_write_char_at_cursor:
+	pusha
+
+	mov ah, 0Ah
+	mov al, 48h
+	int 10h
+
+	popa
+	ret
+
+
+
+
+kernel_load db "Welcome to JobOS", 0
 
 
 times 512-($-$$) db 0
